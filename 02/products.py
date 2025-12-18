3ab1ae780a7523deada336c2e2893b4382fdfb1a
Magne Halvorsen <mh95244719@protonmail.com>
2025-12-18 08:04:20 +0100

    AOC2025 Day 02, Part 2

--- 02/products.py
+++ 02/products.py
@@ -1,4 +1,21 @@
 
+from typing import List
+
+
+def id_str_invalid(id_str: str) -> bool:
+    half = len(id_str)//2
+    for size in range(1, half+1):
+        # Split string into chunks of size size
+        chunks = [id_str[i:i + size] for i in range(0, len(id_str), size)]
+        for chunk in chunks:
+            if chunks[0] != chunk:
+                break
+        else:
+            return True
+    return False
+        
+
+
 def main():
     id_ranges = []
     with open('02/input.txt') as file:
@@ -16,12 +33,8 @@ def main():
         end = int(id_range['end'])
         for id in range(start, end+1):
             id_str = str(id)
-            if len(id_str) % 2 == 1:
-                continue
-            half = len(id_str)//2
-            left, right = id_str[:half], id_str[half:]
-            if left == right:
-                id_sum = id_sum + id
+            if id_str_invalid(id_str):
+                id_sum += id
     print(id_sum)
 
 if __name__ == "__main__":
